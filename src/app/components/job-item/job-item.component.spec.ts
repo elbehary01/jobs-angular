fc8b1982de64ad63c7c4e59e20970f94fb46f551
Elbehary <ahmedelbehary0@gmail.com>
2019-11-27 12:32:10 +0100

    remove the old test and write new

--- src/app/components/job-item/job-item.component.spec.ts
+++ src/app/components/job-item/job-item.component.spec.ts
@@ -1,10 +1,55 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { JobItemComponent } from './job-item.component';
+// import { TestBed, tick, fakeAsync, inject, ComponentFixture } from "@angular/core/testing";
+// // import { AppComponent } from "./app.component";
+// import { Router } from "@angular/router";
+// // import { JobsComponent } from "./components/jobs/jobs.component";
+// // import { JobDetailsComponent } from "./components/job-details/job-details.component";
+// // import { RouterTestingModule } from "@angular/router/testing";
+// // import { routes } from "./app-routing.module";
+// import { Location } from "@angular/common";
+// import { HttpClientModule } from "@angular/common/http";
+// import {
+//   HttpClientTestingModule,
+//   HttpTestingController
+// } from "@angular/common/http/testing";
 
-describe('JobItemComponent', () => {
-  let component: JobItemComponent;
-  let fixture: ComponentFixture<JobItemComponent>;
+// import {JobsService } from "../../services/jobs.service"
+
+// import { JobItemComponent } from './job-item.component';
+// import { async } from 'q';
+// import { DebugElement } from '@angular/core';
+// import { By } from 'protractor';
+
+// describe("JobItemComponent", () => {
+//   let component: JobItemComponent;
+//   // let router: Router;
+//   let fixture: ComponentFixture<JobItemComponent>;
+//   let debugElement:DebugElement
+
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({
+//       imports: [],
+//       declarations: [
+//         JobItemComponent,
+//       ],
+//       providers: [JobsService]
+//     });
+
+//     // router = TestBed.get(Router);
+//     // location = TestBed.get(Location);
+
+//     fixture = TestBed.createComponent(JobItemComponent);
+//     component = fixture.componentInstance;
+
+//   });
+
+//   it("should hide contents if show is false", () => {
+//     const fixture = TestBed.createComponent(JobItemComponent);
+//     fixture.detectChanges();
 
 
-});
+
+//   });
+
+// });
+
