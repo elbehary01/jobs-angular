2e2fab1492528ef35a92da4f8cc12cc92f02814b
Elbehary <ahmedelbehary0@gmail.com>
2019-12-01 18:57:22 +0100

    write test for navBar in the app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,60 +1,69 @@
-import { TestBed, tick, fakeAsync, inject } from '@angular/core/testing';
-import { AppComponent } from './app.component';
-import { Router } from '@angular/router';
-import { JobsComponent } from './components/jobs/jobs.component';
-import { JobDetailsComponent } from './components/job-details/job-details.component';
-import { RouterTestingModule } from '@angular/router/testing';
-import { routes } from './app-routing.module';
-import {Location} from "@angular/common"
-import { JobItemComponent } from './components/job-item/job-item.component';
-import { HttpClientModule } from '@angular/common/http';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { JobsService } from './services/jobs.service';
+import { TestBed, tick, fakeAsync, inject, async } from "@angular/core/testing";
+import { AppComponent } from "./app.component";
+import { Router } from "@angular/router";
+import { JobsComponent } from "./components/jobs/jobs.component";
+import { JobDetailsComponent } from "./components/job-details/job-details.component";
+import { RouterTestingModule } from "@angular/router/testing";
+import { routes } from "./app-routing.module";
+import { Location } from "@angular/common";
+import { JobItemComponent } from "./components/job-item/job-item.component";
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from "@angular/common/http/testing";
+import { JobsService } from "./services/jobs.service";
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
-
-
-describe('Router: App', () => {
-
+describe("Router: App", () => {
   let location: Location;
   let router: Router;
   let fixture;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule.withRoutes(routes), HttpClientTestingModule],
-      declarations: [
-      JobsComponent,
-      JobItemComponent,
-      JobDetailsComponent,
-      AppComponent
+      imports: [
+        RouterTestingModule.withRoutes(routes),
+        HttpClientTestingModule
       ],
-       providers: [
-         JobsService
-       ]
+      declarations: [
+        JobsComponent,
+        JobItemComponent,
+        JobDetailsComponent,
+        AppComponent,
+        NavbarComponent
+      ],
+      providers: [JobsService]
     });
 
-  router = TestBed.get(Router);
-  location = TestBed.get(Location);
+    router = TestBed.get(Router);
+    location = TestBed.get(Location);
 
-  fixture = TestBed.createComponent(AppComponent);
-  router.initialNavigation();
+    fixture = TestBed.createComponent(AppComponent);
+    router.initialNavigation();
   });
 
-  it('should get the from ApiJobs', inject([HttpTestingController, JobsService],
+  it("should get the from ApiJobs", inject(
+    [HttpTestingController, JobsService],
     (httpMock: HttpTestingController, jobService: JobsService) => {
       expect(jobService).toBeTruthy();
     }
-  ))
+  ));
 
-  it('should navigate to Jobs page', fakeAsync(() => {
-    router.navigate(['']);
+  it("should navigate to Jobs page", fakeAsync(() => {
+    router.navigate([""]);
     tick();
-    expect(location.path()).toBe('/');
+    expect(location.path()).toBe("/");
   }));
 
-  it('should navigate to JobDetails', fakeAsync(() => {
-    router.navigate(['/job/:id']);
+  it("should navigate to JobDetails", fakeAsync(() => {
+    router.navigate(["/job/:id"]);
     tick();
-    expect(location.path()).toBe('/job/:id');
+    expect(location.path()).toBe("/job/:id");
+  }));
+
+  it("should create NavBar", async(() => {
+    const fixture = TestBed.createComponent(NavbarComponent);
+    const NavBar = fixture.debugElement.componentInstance;
+    expect(NavBar).toBeTruthy();
   }));
 });
